ea66b4ac48459252527b86c24a1aa48b86d050eb
Patrick Mackridge <38439947+PatrickMackridge@users.noreply.github.com>
2023-03-22 18:52:25 +0000

    Upload empty config file

---
+++ config.py
@@ -0,0 +1,13 @@
+ssid = 'Wifi Network'
+
+password = 'Wifi Password'
+
+url = 'Pushbullet Url'
+
+auth_token = 'Pushbullet Auth token'
+
+device_iden = 'Pushbullet device iden (to send notifs to)'
+
+notif_title = 'Notification Title'
+
+notif_body = 'Notification message body'
